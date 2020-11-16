c8f937e90101a268e84f8c6deddfe2e1de2ec3ec
James <jamesprivett@hotmail.co.uk>
2020-11-16 17:56:53 +0000

    Added constructor and ability to add new client to the hub

--- messaging/hub.go
+++ messaging/hub.go
@@ -1,6 +1,29 @@
 package messaging
 
+import (
+	"log"
+	"net"
+)
+
 type hub struct {
 	clients  map[uint64]*client
 	messages chan message
+}
+
+// NewHub Returns a hub reference
+func NewHub() *hub {
+	return &hub{
+		clients:  make(map[uint64]*client),
+		messages: make(chan message),
+	}
+}
+
+func (h *hub) NewClient(conn net.Conn) {
+	log.Printf("New Client has connected: %s", conn.RemoteAddr().String())
+
+	c := &client{
+		conn: conn,
+		//userID: uint64,// random number?
+		messages: h.messages,
+	}
 }
