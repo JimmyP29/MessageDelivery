e5ea4af44699c647663d316982084cb065f4d026
James <jamesprivett@hotmail.co.uk>
2020-11-16 18:19:30 +0000

    Initial read and write for client

--- messaging/client.go
+++ messaging/client.go
@@ -1,9 +1,57 @@
 package messaging
 
-import "net"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
 
 type client struct {
 	conn     net.Conn
 	userID   uint64
 	messages chan<- message
+}
+
+func (c *client) ReadInput() {
+	for {
+		msg, err := bufio.NewReader(c.conn).ReadString('\n')
+		if err != nil {
+			return
+		}
+
+		msg = strings.Trim(msg, "\r\n")
+
+		args := strings.Split(msg, " ")
+		messages := strings.TrimSpace(args[0])
+
+		fmt.Printf("messages: %v", messages)
+		switch messages {
+		case "/identity":
+			c.messages <- message{
+				msgType: identity,
+				client:  c,
+			}
+		case "/list":
+			c.messages <- message{
+				msgType: list,
+				client:  c,
+			}
+		case "/relay":
+			c.messages <- message{
+				msgType: relay,
+				client:  c,
+			}
+		default:
+			c.err(fmt.Errorf("Unknown command: %s", messages))
+		}
+	}
+}
+
+func (c *client) err(err error) {
+	c.conn.Write([]byte("Error: " + err.Error() + "\n"))
+}
+
+func (c *client) WriteMessage(msg *message) {
+	c.conn.Write([]byte("testing" + msg.body))
 }
