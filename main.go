9293932acd327f72711feeda8e38cfbb9b853d86
James <jamesprivett@hotmail.co.uk>
2020-11-17 17:52:43 +0000

    Websocket working

--- main.go
+++ main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println("New client connected")
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Printf("New message from client: %s", p)
+	}
+}
+
 func main() {
-	fmt.Println("Working...")
+	http.HandleFunc("/ws", websocketHandler)
+	http.ListenAndServe(":8888", nil)
+	fmt.Println("Server running...")
 }
