7be83032fae8b224667818fe78ef7eeaf0afd6ab
James <jamesprivett@hotmail.co.uk>
2020-11-17 22:24:15 +0000

    message struct implemented

--- messaging/message.go
+++ messaging/message.go
@@ -1,1 +1,17 @@
 package messaging
+
+type messageType int
+
+const (
+	identity messageType = iota
+	list
+	relay
+)
+
+// Message - used by the Hub to send to clients
+type Message struct {
+	MsgType   messageType `json:"type"`
+	Body      string      `json:"body"`
+	SenderID  uint64      `json:"senderID"`
+	ClientIDS []uint64    `json:"clientIDS"`
+}
