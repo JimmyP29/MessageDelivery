0787f0d015c7b4213543a414779544b484985199
James <jamesprivett@hotmail.co.uk>
2020-11-19 21:50:20 +0000

    Changed messageType's to be exportable and added comments

--- messaging/message.go
+++ messaging/message.go
@@ -5,9 +5,14 @@ import "encoding/json"
 type messageType int
 
 const (
-	identity messageType = iota
-	list
-	relay
+	// Identity - Used to return the client's userID back to them
+	Identity messageType = iota
+
+	// List - Used to return a list of all connected userID's (excluding the requesting client)
+	List
+
+	// Relay - Used to relay a given message body to selected receivers in the message
+	Relay
 )
 
 // Message - used by the Hub to send to clients
