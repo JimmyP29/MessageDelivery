a6f738dd5b9d6cea2fe435e8998c2f2c6fe89dd7
James <jamesprivett@hotmail.co.uk>
2020-11-21 19:17:00 +0000

    Validation test

---
+++ messaging/validation_test.go
@@ -0,0 +1,26 @@
+package messaging
+
+import "testing"
+
+type ValidateRequestResult struct {
+	subs   []Subscription
+	body   string
+	okSubs bool
+	okBody bool
+}
+
+var validateRequestResults = []ValidateRequestResult{
+	{make([]Subscription, 100, 100), "foobar", true, true},
+}
+
+func TestValidateRequest(t *testing.T) {
+	for _, test := range validateRequestResults {
+		okSubs, okBody := ValidateRequest(test.subs, test.body)
+		if okSubs != test.okSubs {
+			t.Fatal("Expected result for subs not given")
+		}
+		if okBody != test.okBody {
+			t.Fatal("Expected result for body not given")
+		}
+	}
+}
