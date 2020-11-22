8eddb72c65bc4f2414861e088b94215e6b5ee6e9
James <jamesprivett@hotmail.co.uk>
2020-11-22 21:13:22 +0000

    Unit tests wip

--- messaging/hub_test.go
+++ messaging/hub_test.go
@@ -1,6 +1,8 @@
 package messaging
 
-import "testing"
+import (
+	"testing"
+)
 
 var t = "test"
 var c = CreateConnection()
@@ -12,6 +14,11 @@ var client1 = Client{
 
 var client2 = Client{
 	userID:     456,
+	connection: &c,
+}
+
+var client3 = Client{
+	userID:     789,
 	connection: &c,
 }
 
@@ -54,6 +61,44 @@ var newHubResults = []NewHubResult{
 // 	}
 // }
 
+type AddClientResult struct {
+	client   Client
+	expected *Hub
+}
+
+var addClientResults = []AddClientResult{
+	{client3, &hub},
+}
+
+func TestAddClient(t *testing.T) {
+	for _, test := range addClientResults {
+		h := hub.AddClient(test.client)
+
+		if h != test.expected {
+			t.Fatalf("Expected result: %v \n Actual result: %v\n", test.expected, h)
+		}
+	}
+}
+
+type GetSubscriptionsResult struct {
+	client   *Client
+	expected []Subscription
+}
+
+var getSubscriptionsResults = []GetSubscriptionsResult{
+	{&hub.clients[0], hub.subscriptions},
+}
+
+func TestGetSubscriptions(t *testing.T) {
+	for _, test := range getSubscriptionsResults {
+		subs := hub.getSubscriptions(test.client)
+
+		if subs[0].client.userID != test.expected[0].client.userID {
+			t.Fatalf("Expected result: %v \n Actual result: %v\n", test.expected[0].client.userID, subs[0].client.userID)
+		}
+	}
+}
+
 type GetRequestedSubscriptionsResult struct {
 	ids      []uint64
 	expected []Subscription
@@ -69,6 +114,151 @@ func TestGetRequestedSubscriptions(t *testing.T) {
 
 		if subs[0].client.userID != test.expected[0].client.userID {
 			t.Fatalf("Expected result: %v \n Actual result: %v\n", test.expected[0].client.userID, subs[0].client.userID)
+		}
+	}
+}
+
+type PublishToSenderResult struct {
+	message  []byte
+	client   *Client
+	expected bool
+}
+
+var publishToSenderResults = []PublishToSenderResult{
+	{[]byte{34, 102, 111, 111, 98, 97, 114, 34}, &hub.clients[0], true},
+}
+
+// func TestPublishToSender(t *testing.T) {
+// 	for _, test := range publishToSenderResults {
+// 		isOK := hub.publishToSender(test.message, test.client)
+
+// 		if isOK != test.expected {
+// 			t.Fatalf("Expected result: %v \n Actual result: %v\n", test.expected, isOK)
+// 		}
+// 	}
+// }
+
+type PublishToReceiversResult struct {
+	message  []byte
+	subs     []Subscription
+	expected bool
+}
+
+var publishToReceiverResults = []PublishToReceiversResult{
+	{[]byte{34, 102, 111, 111, 98, 97, 114, 34}, subs, true},
+}
+
+// func TestPublishToReceivers(t *testing.T) {
+// 	for _, test := range publishToReceiverResults {
+// 		isOK := hub.publishToReceivers(test.message, test.subs)
+
+// 		if isOK != test.expected {
+// 			t.Fatalf("Expected result: %v \n Actual result: %v\n", test.expected, isOK)
+// 		}
+// 	}
+// }
+
+type HandleIdentityResult struct {
+	client   *Client
+	expected bool
+}
+
+var handleIdentityResults = []HandleIdentityResult{
+	{&hub.clients[0], true},
+}
+
+// func TestHandleIdentity(t *testing.T) {
+// 	for _, test := range handleIdentityResults {
+// 		isOK := hub.handleIdentity(test.client)
+
+// 		if isOK != test.expected {
+// 			t.Fatalf("Expected result: %v \n Actual result: %v\n", test.expected, isOK)
+// 		}
+// 	}
+// }
+
+type HandleListResult struct {
+	client   *Client
+	expected bool
+}
+
+var handleListResults = []HandleListResult{
+	{&hub.clients[0], true},
+}
+
+// func TestHandleList(t *testing.T) {
+// 	for _, test := range handleListResults {
+// 		isOK := hub.handleList(test.client)
+
+// 		if isOK != test.expected {
+// 			t.Fatalf("Expected result: %v \n Actual result: %v\n", test.expected, isOK)
+// 		}
+// 	}
+// }
+
+var message = Message{
+	MsgType:   2,
+	Body:      []byte{34, 102, 111, 111, 98, 97, 114, 34},
+	ClientIDS: []uint64{456},
+}
+
+type HandleRelayResult struct {
+	client   *Client
+	message  *Message
+	expected bool
+}
+
+var handleRelayResults = []HandleRelayResult{
+	{&hub.clients[0], &message, true},
+}
+
+// func TestHandleRelay(t *testing.T) {
+// 	for _, test := range handleRelayResults {
+// 		isOK := hub.handleRelay(*test.client, *test.message)
+
+// 		if isOK != test.expected {
+// 			t.Fatalf("Expected result: %v \n Actual result: %v\n", test.expected, isOK)
+// 		}
+// 	}
+// }
+
+type HandleDefaultResult struct {
+	client   *Client
+	expected bool
+}
+
+var handleDefaultResults = []HandleDefaultResult{
+	{&hub.clients[0], true},
+}
+
+// func TestHandleDefault(t *testing.T) {
+// 	for _, test := range handleDefaultResults {
+// 		isOK := hub.handleDefault(test.client)
+
+// 		if isOK != test.expected {
+// 			t.Fatalf("Expected result: %v \n Actual result: %v\n", test.expected, isOK)
+// 		}
+// 	}
+// }
+
+//var p = SerialiseMessage(&message)
+
+type HandleReceiveMessageResult struct {
+	client   *Client
+	payload  []byte
+	expected *Hub
+}
+
+var handleReceiveMessageResults = []HandleReceiveMessageResult{
+	{&hub.clients[0], []byte{34, 102, 111, 111, 98, 97, 114, 34}, &hub},
+}
+
+func TestHandleReceiveMessage(t *testing.T) {
+	for _, test := range handleReceiveMessageResults {
+		h := hub.HandleReceiveMessage(*test.client, test.payload)
+
+		if h != test.expected {
+			t.Fatalf("Expected result: %v \n Actual result: %v\n", test.expected, h)
 		}
 	}
 }
